b990d07ab02de8e78b27562868ae9b5a00314991
dara-rara <glebova555dasha@gmail.com>
2024-09-13 16:41:52 +0500

    exercise1.1

--- base/mod1/src/Main.java
+++ base/mod1/src/Main.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        var sum = 0;
+        for (var i = 0; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) sum += i;
         }
+        out.println("Сумма чисел от 0 до 1000, кратных 3 или 5: " + sum);
     }
 }
