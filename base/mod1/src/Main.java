eb6f1edafce0623d993eb1ce9990707f230a1895
dara-rara <glebova555dasha@gmail.com>
2024-09-13 16:52:33 +0500

    exercise1.2

--- base/mod1/src/Main.java
+++ base/mod1/src/Main.java
@@ -2,10 +2,28 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+    }
+
+    private static void exerciseTwo() {
+        int[][] arr = { {20, 34, 2}, {9, 12, 18}, {3, 4, 5} };
+        var min = arr[0][0];
+
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < arr[i].length; j++) {
+                min = Integer.min(min, arr[i][j]);
+            }
+        }
+        System.out.println("2. Минимальный элемент массива: " + min);
+    }
+
+    private static void exerciseOne() {
         var sum = 0;
         for (var i = 0; i <= 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) sum += i;
         }
-        out.println("Сумма чисел от 0 до 1000, кратных 3 или 5: " + sum);
+        out.println("1. Сумма чисел от 0 до 1000, кратных 3 или 5: " + sum);
     }
+
 }
